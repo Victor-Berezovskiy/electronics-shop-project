afe76b90bec36810288a3d3edd36babd593d08be
Victor-Berezovskiy <berezovskiy966@gmail.com>
2023-03-17 17:05:03 +0300

    HW done privat and classmethod

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -5,19 +8,59 @@ class Item:
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, __name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
 
-        :param name: Название товара.
+        :param __name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = __name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
+        self.all.append(self)
 
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, new_name: str) -> None:
+        if len(new_name) > 10:
+            raise Exception("Длина наименования товара превышает 10 символов.")
+        self.__name = new_name
+
+    @classmethod
+    def instantiate_from_csv(cls, csv_path='../src/items.csv'):
+        with open(csv_path) as file:
+            file_reader = csv.DictReader(file, delimiter=',')
+            for i in file_reader:
+                name, price, quantity = i.get('name'), int(i.get('price')), int(i.get('quantity'))
+                cls.all.append((name, price, quantity))
+
+    @classmethod
+    def set_pay_rate(cls, new_rate):
+
+        if 0 <= new_rate:
+            cls.__pay_rate = new_rate
+        else:
+            raise ValueError("Значение индексации не может быть отрицательным или равным '0'")
+
+    @staticmethod
+    def string_to_number(number: str) -> int:
+        """Cтатический метод, возвращающий число из числа-строки
+        :return: Число в нужном нам формате
+        """
+        return int(number.split(".")[0])
+
+    def __str__(self):
+        """Тандер для вывода информации об экземпляре"""
+        return f"{self.__name}"
+
+    def __repr__(self):
+        """Тандер для вывода информации об экземпляре"""
+        return f"Item{self.__name, self.price, self.quantity}"
 
     def calculate_total_price(self) -> float:
         """
@@ -28,7 +71,7 @@ class Item:
         return total_price
 
     def item_info(self):
-        return f'Общая стоимость товара {self.name} = {self.price}'
+        return f'Общая стоимость товара {self.__name} = {self.price}'
 
     def apply_discount(self) -> None:
         """
@@ -36,14 +79,4 @@ class Item:
         """
         self.price = self.price * Item.pay_rate
         return self.price
-
-
-
-
-
-
-
-
-
-
 
--- tests/test_item.py
+++ tests/test_item.py
@@ -8,11 +8,11 @@ Item.pay_rate = 0.8
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 def test_calculate_total_price():
 
-    assert item1.name == "Смартфон"
+    assert item1.__name == "Смартфон"
     assert item1.price == 10000
     assert item1.quantity == 20
 
-    assert item2.name == "Ноутбук"
+    assert item2.__name == "Ноутбук"
     assert item2.price == 20000
     assert item2.quantity == 5
 
@@ -21,3 +21,5 @@ def test_apply_discount():
     assert item1.price == 8000.0
     item2.price = item2.price * Item.pay_rate
     assert item2.price == 16000
+
+    
